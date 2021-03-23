a468e2144b95c88c76913642ca00fff94c222a74
IkerSanJuan <isanjuanr1900@alumno.ipn.mx>
2021-03-22 19:58:56 -0600

    return validar()

---
+++ JS/validar.js
@@ -0,0 +1,12 @@
+function validar(formulario){
+    /*Queremos validad
+     que se escriban mas 
+     de 5 caracteres en el nombre
+    */
+
+     if (formulario.nombre.value.length < 5){
+        alert("Por favor escribe mas de 5 caracteres en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+     }
+}
